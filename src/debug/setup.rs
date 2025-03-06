2a896b828bebddce70a91ef3a3b6c23a02f0e484
caffidev <caffidev@gmail.com>
2025-03-06 20:07:55 +1000

    feat: primary monitor

--- src/debug/setup.rs
+++ src/debug/setup.rs
@@ -52,6 +52,7 @@ pub fn setup_hud(mut commands: Commands, query: Query<(Entity, &Monitor, Has<Pri
     // Gets monitor info
     for (_, monitor, is_primary) in query.iter() {
 	let mut monitor_info = monitor.name.clone().unwrap_or_else(|| "undefined".to_string());
+	if is_primary {monitor_info.push_str("(primary) ");}
 	monitor_info.push_str(&format!("({}x{}, {} hz) ",
 				       monitor.physical_width.clone(),
 				       monitor.physical_height.clone(),
