1ecb75eb0663e10440d288867e401976621f6e7c
caffidev <caffidev@gmail.com>
2025-04-19 00:18:01 +1000

    feat: pause menu

--- src/gui/menu/pause.rs
+++ src/gui/menu/pause.rs
@@ -1,28 +1,53 @@
 use bevy::prelude::*;
 
-/** Marker to find pause menu hud box entity. */
+/** Marker to find pause menu background entity. */
 #[derive(Component)]
-pub struct PauseHudRoot;
+pub struct PauseMenu;
+
+#[derive(Component)]
+pub enum PauseButtonAction {
+    Resume,
+    Exit,
+}
 
 /** Draws and setups pause menu. */
 pub fn setup_pause_menu(mut commands: Commands) {
-    let hud_root = commands
+    let pause_menu = commands
         .spawn((
-            PauseHudRoot,
+            PauseMenu,
+	    Name::new("PauseMenu"),
             (
-                BackgroundColor(Color::BLACK.with_alpha(0.5)),
-                GlobalZIndex(i32::MAX),
-                Node {
-                    position_type: PositionType::Absolute,
-                    left: Val::Percent(100.),
-                    top: Val::Percent(100.),
-                    ..Default::default()
-                },
+                BackgroundColor(Color::BLACK.with_alpha(0.6)),
+                GlobalZIndex(5),
+                (
+		    Node {
+			width: Val::Vw(100.),
+			height: Val::Vh(100.),
+			display: Display::Flex,
+			align_items: AlignItems::Center,
+			justify_content: JustifyContent::Center,
+			..Default::default()
+                    },
+		)
             ),
         ))
         .id();
 
-    let text_pause = commands
+    let pause_gui_root = commands
+	.spawn(
+	    Node {
+		display: Display::Flex,
+		flex_direction: FlexDirection::Column,
+		align_items: AlignItems::Center,
+		justify_content: JustifyContent::SpaceAround,
+		height: Val::Vh(40.),
+		min_width: Val::Vw(40.),
+		..Default::default()
+	    }
+	)
+	.id();
+    
+    let pause_text = commands
         .spawn((
             Text::new("Pause menu"),
             TextFont::from_font_size(16.0),
@@ -30,5 +55,40 @@ pub fn setup_pause_menu(mut commands: Commands) {
         ))
         .id();
 
-    commands.entity(hud_root).add_children(&[text_pause]);
+    commands.entity(pause_menu).add_children(&[pause_gui_root]);
+    commands.entity(pause_gui_root).add_children(&[pause_text]);
+
+        commands.entity(pause_gui_root)
+	.with_children(|wrapper| {
+	    for (msg, action) in [
+		("Resume", PauseButtonAction::Resume),
+		("Exit", PauseButtonAction::Exit),
+	    ] {
+		wrapper.spawn((
+                    action,
+                    (
+                        Button,
+                        Node {
+                            width: Val::Percent(100.),
+                            border: UiRect::all(Val::Px(3.)),
+                            display: Display::Flex,
+                            flex_direction: FlexDirection::Row,
+                            align_items: AlignItems::Center,
+                            justify_content: JustifyContent::Center,
+                            padding: UiRect::all(Val::Px(7.)),
+                            ..Default::default()
+                        },
+                        BackgroundColor(Color::srgb(0.3, 0.3, 0.3)),
+                        BorderColor(Color::BLACK),
+                    ),
+                ))
+                .with_children(|button| {
+                    button.spawn((
+                        Text::new(msg),
+                        TextFont::from_font_size(16.),
+                        TextColor(Color::WHITE),
+                    ));
+                });
+	    }
+	}).id();
 }
