5c7a9517d319ce82965550947deca0aa3a184bea
caffidev <caffidev@gmail.com>
2025-04-19 15:08:37 +1000

    feat: creating default settings.toml

--- src/settings.rs
+++ src/settings.rs
@@ -1,7 +1,7 @@
 use bevy::window::PrimaryWindow;
 use bevy::{prelude::*, window::WindowMode};
 
-use serde::Deserialize;
+use serde::{Serialize, Deserialize};
 use toml::from_str;
 
 use crate::gui::GUIScale;
@@ -9,7 +9,7 @@ use std::error::Error;
 use std::path::PathBuf;
 
 #[allow(dead_code)]
-#[derive(Deserialize, Clone, Copy, Resource, Debug)]
+#[derive(Deserialize, Serialize, Clone, Copy, Resource, Debug)]
 pub struct Settings {
     pub seed: u64,
     pub gui_scale: f32,
@@ -22,7 +22,7 @@ impl Default for Settings {
     fn default() -> Self {
         Self {
             seed: 0,
-            gui_scale: 0.1,
+            gui_scale: 0.,
             fullscreen: false,
             pause_on_lost_focus: true,
             mute_on_lost_focus: true,
@@ -64,13 +64,33 @@ pub fn setup_settings(
 ) {
     match read_settings((*settings_path.path).to_path_buf(), &mut settings) {
         Ok(()) => info!("{:?}", *settings),
-        Err(e) => warn!("Couldn't retrieve settings: {}", e),
+        Err(e) => {
+	    let path: &str = settings_path.path.to_str().unwrap();
+	    #[cfg(debug_assertions)]
+	    let debug : bool = true;
+	    #[cfg(not(debug_assertions))]
+	    let debug : bool = false;
+	    
+	    warn!("Couldn't retrieve settings from {}: {}.", path, e);
+	    if debug {
+		warn!("Debug mode enabled, settings file is not created, fallback values in use.");
+	    }
+	    else {
+		warn!("Creating default settings file...");
+		match write_settings((*settings_path.path).to_path_buf(), &mut settings) {
+		    Ok(()) => info!("Default settings file was created."),
+		    Err(e) => {
+			warn!("Default settings file can't be created: {}", e);
+		    }
+		}
+	    }
+	}
     }
 
     let gui_scale = f32::floor(settings.gui_scale);
 
-    // Protection from negative value
-    if settings.gui_scale < 0.2 {
+    // Too small/negative values.
+    if settings.gui_scale < 0.5 {
         commands.insert_resource(GUIScale::Auto(0));
     } else if settings.gui_scale == gui_scale {
         commands.insert_resource(GUIScale::Scale(gui_scale as u8));
@@ -83,11 +103,19 @@ pub fn setup_settings(
     }
 }
 
+pub fn write_settings(file_path: PathBuf, settings: &mut Settings) -> Result<(), Box<dyn Error>> {
+    let settings_toml = toml::to_string(*&settings)?;
+
+    std::fs::write(file_path, settings_toml)?;
+    
+    Ok(())
+}
+
 pub fn read_settings(file_path: PathBuf, settings: &mut Settings) -> Result<(), Box<dyn Error>> {
     debug!("Path to settings: {:?}", std::fs::canonicalize(&file_path));
 
     let settings_str = std::fs::read_to_string(file_path)?;
-
+    
     *settings = from_str(&settings_str)?;
 
     Ok(())
