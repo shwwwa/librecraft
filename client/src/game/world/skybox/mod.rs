891ec2ab1e8504a1fe264dc09e2a8f10bcdd3f46
= <shwwwa.dev@gmail.com>
2025-05-01 15:35:22 +1000

    feat: shader handle revert

--- client/src/game/world/skybox/mod.rs
+++ client/src/game/world/skybox/mod.rs
@@ -1,11 +1,5 @@
 use bevy::asset::{load_internal_asset, weak_handle};
 use bevy::prelude::*;
-use bevy::render::view::RenderLayers;
-
-/// The `Handle` for the shader for the `SkyboxMaterial`
-/// Generated with `Uuid::new_v4()`
-pub const SKYBOX_SHADER_HANDLE: Handle<Shader> =
-    weak_handle!("7c1959f7-9ad5-4c78-b7f6-fd57e1e1a76a");
 
 #[derive(Debug, Clone, Copy)]
 pub struct SkyboxPlugin;
@@ -27,13 +21,16 @@ impl Plugin for SkyboxPlugin {
 /// When added, a `Skybox` will be created as a child.
 /// When removed, that `Skybox` will also be removed.
 #[derive(Component, Default, Debug, Clone)]
-pub struct SkyboxCamera {
-    /// Controls whether or not the skybox will be seen only on certain render layers.
-    pub render_layers: Option<RenderLayers>,
-}
+pub struct SkyboxCamera;
 
 /// A marker `Component` for skybox entities.
 ///
 /// Added for the skybox generated when an `SkyboxCamera` is detected.
 #[derive(Component, Debug, Clone, Copy)]
 pub struct Skybox;
+
+/// The `Handle` for the shader for the [`SkyboxMaterial`]
+///
+/// Generated with `Uuid::new_v4()`
+pub const SKYBOX_SHADER_HANDLE: Handle<Shader> =
+    weak_handle!("7c1959f7-9ad5-4c78-b7f6-fd57e1e1a76a");
